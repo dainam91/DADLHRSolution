0d5ba966a36c4908fa19955baab827c1ac11066a
dainam91 <tranphandainam91@gmail.com>
2020-07-13 10:56:05 +0700

    Add Entity

---
+++ DADLHRSolution.Data/EF/DADLHRContext.cs
@@ -0,0 +1,17 @@
+﻿using DADLHRSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DADLHRSolution.Data.EF
+{
+    public class DADLHRContext : DbContext
+    {
+        public DADLHRContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<WorkType> WorkTypes { get; set; }
+    }
+}
